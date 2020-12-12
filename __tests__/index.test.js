c52fa78af316a81c3213e91d1507aae6af3f9258
ArtemProvornyy <artem.provornyi@gmail.com>
2020-12-13 00:18:38 +0200

    add tests for json formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import fs from 'fs';
-import genDiff, { getData } from '..';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,10 +12,12 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 let expectedRecursive;
 let expectedPlain;
+let expectedJson;
 
 beforeEach(() => {
   expectedRecursive = readFile('recursive.txt');
   expectedPlain = readFile('plain.txt');
+  expectedJson = readFile('json.txt');
 });
 
 describe('Generate difference of two files', () => {
@@ -25,16 +27,25 @@ describe('Generate difference of two files', () => {
   test('plain.json', () => {
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(expectedPlain);
   });
+  test('json.json', () => {
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(expectedJson);
+  });
   test('recursive.yml', () => {
     expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toEqual(expectedRecursive);
   });
   test('plain.yml', () => {
     expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(expectedPlain);
   });
+  test('json.yml', () => {
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(expectedJson);
+  });
   test('recursive.ini', () => {
     expect(genDiff(getFixturePath('file1.ini'), getFixturePath('file2.ini'), 'stylish')).toEqual(expectedRecursive);
   });
   test('plain.ini', () => {
     expect(genDiff(getFixturePath('file1.ini'), getFixturePath('file2.ini'), 'plain')).toEqual(expectedPlain);
+  });
+  test('json.ini', () => {
+    expect(genDiff(getFixturePath('file1.ini'), getFixturePath('file2.ini'), 'json')).toEqual(expectedJson);
   });
 });
