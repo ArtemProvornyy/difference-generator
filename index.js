e6cd382a2f86e46a78c72535d366353b1e85a6c5
ArtemProvornyy <artem.provornyi@gmail.com>
2020-12-23 21:50:35 +0200

    rename constants and function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import getParser from './src/parsers.js';
 import buildAST from './src/gendiff.js';
 import switchFormat from './src/formatters/index.js';
 
-const getData = (filepath) => {
+const getContent = (filepath) => {
   const cwd = process.cwd();
   const absFilepath = path.resolve(cwd, filepath);
 
@@ -12,13 +12,13 @@ const getData = (filepath) => {
 };
 
 export default (filepath1, filepath2, format) => {
-  const file1 = getData(filepath1);
-  const file2 = getData(filepath2);
+  const content1 = getContent(filepath1);
+  const content2 = getContent(filepath2);
 
   const parse = getParser(filepath1);
 
-  const file1Obj = parse(file1);
-  const file2Obj = parse(file2);
+  const file1Obj = parse(content1);
+  const file2Obj = parse(content2);
 
   const formatter = switchFormat(format);
 
