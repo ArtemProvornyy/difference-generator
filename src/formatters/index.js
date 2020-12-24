7417d61b2a9221865e761b6930eeb10a6552c721
ArtemProvornyy <artem.provornyi@gmail.com>
2020-12-24 10:37:44 +0200

    remove default parameter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,7 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-export default (format = 'stylish') => {
+export default (format) => {
   switch (format) {
     case 'stylish':
       return stylish;
