c58c1a16e1db553ca04c85001c6b960308fa6a6b
ArtemProvornyy <artem.provornyi@gmail.com>
2020-12-27 22:58:32 +0200

    update stylish format

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,29 @@
 import _ from 'lodash';
 
-const stringify = (obj, depth) => {
+const stringify = (inputValue, depth) => {
+  if (!_.isPlainObject(inputValue)) {
+    return inputValue;
+  }
+
+  const obj = inputValue;
   const keys = Object.keys(obj);
   const indent = ' '.repeat(depth);
   const braceIndent = ' '.repeat(depth - 4);
 
   const innerPart = keys.map((key) => {
-    const value = obj[key];
-    if (_.isPlainObject(value)) {
-      return `${indent}${key}: ${stringify(value, depth + 4)}`;
+    const currentValue = obj[key];
+    if (_.isPlainObject(currentValue)) {
+      return `${indent}${key}: ${stringify(currentValue, depth + 4)}`;
     }
 
-    return `${indent}${key}: ${value}`;
+    return `${indent}${key}: ${currentValue}`;
   });
 
   return `{\n${innerPart.join('\n')}\n${braceIndent}}`;
 };
 
-const stylish = (diff) => {
-  const iter = (node, depth) => {
+const renderStylish = (diff) => {
+  const iter = (depth, node) => {
     if (node.lenght === 0) {
       return '';
     }
@@ -28,38 +33,32 @@ const stylish = (diff) => {
         name, value, status, oldValue, children,
       } = child;
       const indent = ' '.repeat(depth);
+      const nextLevelDepth = status === 'nested' ? depth + 4 : depth + 6;
+
       if (status === 'nested') {
-        return `${indent}  ${name}: {\n${iter(children, depth + 4)}\n${indent}  }`.split(',');
+        return `${indent}  ${name}: {\n${iter(nextLevelDepth, children)}\n${indent}  }`.split(',');
       }
-      if (_.isPlainObject(value) && status === 'updated') {
-        return `${indent}- ${name}: ${oldValue}\n${indent}+ ${name}: ${stringify(value, depth + 6)}`;
+      if (status === 'updated') {
+        return `${indent}- ${name}: ${stringify(oldValue, nextLevelDepth)}\n${indent}+ ${name}: ${stringify(value, nextLevelDepth)}`;
       }
-      if (_.isPlainObject(oldValue) && status === 'updated') {
-        return `${indent}- ${name}: ${stringify(oldValue, depth + 6)}\n${indent}+ ${name}: ${value}`;
+      if (status === 'added') {
+        return `${indent}+ ${name}: ${stringify(value, nextLevelDepth)}`;
       }
-      if (_.isPlainObject(value) && status === 'added') {
-        return `${indent}+ ${name}: ${stringify(value, depth + 6)}`;
-      }
-      if (_.isPlainObject(value) && status === 'removed') {
-        return `${indent}- ${name}: ${stringify(value, depth + 6)}`;
+      if (status === 'removed') {
+        return `${indent}- ${name}: ${stringify(value, nextLevelDepth)}`;
       }
       if (status === 'unchanged') {
         return `${indent}  ${name}: ${value}`;
       }
-      if (status === 'added') {
-        return `${indent}+ ${name}: ${value}`;
-      }
-      if (status === 'removed') {
-        return `${indent}- ${name}: ${value}`;
-      }
 
-      return `${indent}- ${name}: ${oldValue}\n${indent}+ ${name}: ${value}`;
+      throw new Error('Unexpected condition. Please check the input data.');
     });
   };
 
-  const innerPart = iter(diff, 2);
+  const startDepth = 2;
+  const innerPart = iter(startDepth, diff);
 
   return `{\n${innerPart.join('\n')}\n}`;
 };
 
-export default stylish;
+export default renderStylish;
