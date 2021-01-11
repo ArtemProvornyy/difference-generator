170dc44eae42ea52be265893b13d4b117c0a2d2d
ArtemProvornyy <artem.provornyi@gmail.com>
2021-01-12 00:48:16 +0200

    add getIndent()

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+const getIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+
 const stringify = (inputValue, depth) => {
   if (!_.isPlainObject(inputValue)) {
     return inputValue;
@@ -7,19 +9,19 @@ const stringify = (inputValue, depth) => {
 
   const obj = inputValue;
   const keys = Object.keys(obj);
-  const indent = ' '.repeat(depth);
-  const braceIndent = ' '.repeat(depth - 4);
+  const indent = getIndent(depth);
+  const braceIndent = getIndent(depth - 1);
 
   const innerPart = keys.map((key) => {
     const currentValue = obj[key];
     if (_.isPlainObject(currentValue)) {
-      return `${indent}${key}: ${stringify(currentValue, depth + 4)}`;
+      return `${indent}  ${key}: ${stringify(currentValue, depth + 1)}`;
     }
 
-    return `${indent}${key}: ${currentValue}`;
+    return `${indent}  ${key}: ${currentValue}`;
   });
 
-  return `{\n${innerPart.join('\n')}\n${braceIndent}}`;
+  return `{\n${innerPart.join('\n')}\n${braceIndent}  }`;
 };
 
 const renderStylish = (diff) => {
@@ -27,18 +29,18 @@ const renderStylish = (diff) => {
     const {
       name, value, status, oldValue, children,
     } = child;
-    const indent = ' '.repeat(depth);
-    const nextLevelDepth = depth + 4;
+    const indent = getIndent(depth);
+    const nextLevelDepth = depth + 1;
 
     switch (status) {
       case 'nested':
         return `${indent}  ${name}: {\n${iter(nextLevelDepth, children)}\n${indent}  }`.split(',');
       case 'updated':
-        return `${indent}- ${name}: ${stringify(oldValue, nextLevelDepth + 2)}\n${indent}+ ${name}: ${stringify(value, nextLevelDepth + 2)}`;
+        return `${indent}- ${name}: ${stringify(oldValue, nextLevelDepth)}\n${indent}+ ${name}: ${stringify(value, nextLevelDepth)}`;
       case 'added':
-        return `${indent}+ ${name}: ${stringify(value, nextLevelDepth + 2)}`;
+        return `${indent}+ ${name}: ${stringify(value, nextLevelDepth)}`;
       case 'removed':
-        return `${indent}- ${name}: ${stringify(value, nextLevelDepth + 2)}`;
+        return `${indent}- ${name}: ${stringify(value, nextLevelDepth)}`;
       case 'unchanged':
         return `${indent}  ${name}: ${value}`;
       default:
@@ -46,7 +48,7 @@ const renderStylish = (diff) => {
     }
   });
 
-  const startDepth = 2;
+  const startDepth = 1;
   const innerPart = iter(startDepth, diff);
 
   return `{\n${innerPart.join('\n')}\n}`;
